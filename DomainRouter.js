49513d5b2d97a7012cc348d0af70b6ff568e8a25
KarolyRobert <karoy.robert.das@gmail.com>
2019-12-16 11:02:53 +0100

    proba

--- DomainRouter.js
+++ DomainRouter.js
@@ -2,25 +2,34 @@
 
 const bouncy = require("bouncy");
 const tls = require("tls");
+//const net = require("net");
 const config = require("webconfigurator")({ router: true });
 
-console.log(process.env);
-
+//console.log(process.env);
+var counter = 0;
 const http = bouncy(function(req, res, bounce) {
   var bounced = false;
   config.http.map(function(host) {
     if (req.headers.host == host.name) {
-      bounced = true;
-      bounce(host.port, { headers: { ishttp: true } });
+      counter++;
+      if((counter % 4) === 0){
+        console.log("host: "+host.name+" visitcount: ",counter/4);
+      } 
+      try{
+          bounce(host.port, { headers: { ishttp: true } });
+          bounced = true;
+      }catch(e){
+        console.log("Error! host:"+host.name,e.toString());
+      }
     }
   });
   if (!bounced) {
     res.statusCode = 404;
-    res.end("no such host");
+    res.end("Az oldal nem elérhető!");
   }
 });
 
-var sslRouters = {
+var sslRouters = config.https.length === 0 ? null : {
   key: config.certs.default.key,
   cert: config.certs.default.cert,
   SNICallback: function(host, cb) {
@@ -29,7 +38,7 @@ var sslRouters = {
   }
 };
 
-const https = bouncy(sslRouters, function(req, res, bounce) {
+const https = config.https.length === 0 ? null : bouncy(sslRouters, function(req, res, bounce) {
   var bounced = false;
   config.https.map(function(host) {
     if (req.headers.host == host.name) {
@@ -44,4 +53,6 @@ const https = bouncy(sslRouters, function(req, res, bounce) {
 
 // ports from router virtualserver configuration
 http.listen(config.httpPort);
-https.listen(config.httpsPort);
+if(config.https.length > 0){
+  https.listen(config.httpsPort);
+}
